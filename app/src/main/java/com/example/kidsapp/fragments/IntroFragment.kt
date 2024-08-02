bc06607ca7b9fb7cd9736ff03dbfbce00e44e5bb
MusaHesenov <musa.hesenov.2018@gmail.com>
2024-08-02 13:38:04 +0400

    Bug Fix

--- app/src/main/java/com/example/kidsapp/fragments/IntroFragment.kt
+++ app/src/main/java/com/example/kidsapp/fragments/IntroFragment.kt
@@ -1,13 +1,13 @@
-package com.example.kidsapp
+package com.example.kidsapp.fragments
 
-import android.media.Image
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.animation.AnimationUtils
-import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
+import com.example.kidsapp.R
 import com.example.kidsapp.databinding.FragmentIntroBinding
 
 class IntroFragment : Fragment(R.layout.fragment_intro_) {
@@ -27,10 +27,15 @@ class IntroFragment : Fragment(R.layout.fragment_intro_) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         //declare the animation
-        val ttb = AnimationUtils.loadAnimation(this.context,R.anim.ttb)
+        val ttb = AnimationUtils.loadAnimation(this.context, R.anim.ttb)
 
         //set the animation
         val introImage1 = binding.imageIntro.startAnimation(ttb)
+
+        binding.registerBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_introFragment_to_createProfileFragment)
+        }
+
     }
 
 
