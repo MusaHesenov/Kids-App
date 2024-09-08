c34069686e468f2227c60fbc8920d2a387a653ce
Elvin <114992009+Elvin5002@users.noreply.github.com>
2024-09-08 11:18:30 +0400

    - add animal viewmodel

---
+++ app/src/main/java/com/example/kidsapp/viewmodel/AnimalViewModel.kt
@@ -0,0 +1,57 @@
+package com.example.kidsapp.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.kidsapp.data.Animal
+import com.example.kidsapp.utils.Resource
+import com.google.firebase.firestore.FirebaseFirestore
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class AnimalViewModel @Inject constructor(
+    private val firestore: FirebaseFirestore
+): ViewModel() {
+    private val _animalList = MutableStateFlow<Resource<List<Animal>>>(Resource.Unspecified())
+    val animalList = _animalList.asStateFlow()
+
+    private val _animalCategoryList = MutableStateFlow<Resource<List<String>>>(Resource.Unspecified())
+    val animalCategoryList = _animalCategoryList.asStateFlow()
+
+    init {
+        fetchAnimalCategoryList()
+    }
+
+    private fun fetchAnimalList(animalType: String) {
+        firestore.collection("category").document("animals").collection("animals").document(animalType).collection(animalType).get()
+            .addOnSuccessListener {
+                val animalList = it.toObjects(Animal::class.java)
+                viewModelScope.launch {
+                    _animalList.emit(Resource.Success(animalList))
+                }
+            }
+            .addOnFailureListener {
+                viewModelScope.launch {
+                    _animalList.emit(Resource.Error(it.message.toString()))
+                }
+            }
+    }
+
+    private fun fetchAnimalCategoryList() {
+        firestore.collection("category").document("animals").collection("animals").get()
+            .addOnSuccessListener {
+                val categoryList = it.toObjects(String::class.java)
+                _animalCategoryList.value = Resource.Success(categoryList)
+
+            }.addOnFailureListener {
+                viewModelScope.launch {
+                    _animalCategoryList.emit(Resource.Error(it.message.toString()))
+                }
+
+            }
+    }
+
+}
